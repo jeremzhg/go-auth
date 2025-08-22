3ebb786943935df2614e79c6ac5025f75011f9bd
Jeremy <jer.auriel@gmail.com>
2025-08-22 10:38:48 +0700

    feat: policies table schema

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/jeremzhg/go-auth
 
 go 1.24.4
+
---
+++ internal/database/db.go
@@ -0,0 +1,12 @@
+package database
+//   createPoliciesTableSQL := `
+// 	CREATE TABLE IF NOT EXISTS policies (
+//     id SERIAL PRIMARY KEY,
+//     subject TEXT NOT NULL,
+//     object TEXT NOT NULL,
+//     action TEXT NOT NULL,
+//     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+//     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+//     UNIQUE (subject, object, action)
+// );`
