7ea5357d4b0aa66677d12774a37d4a64b4fa555e
Jeremy <jer.auriel@gmail.com>
2025-08-22 10:53:19 +0700

    fix: check errors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -11,7 +12,12 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+		if _, err := w.Write([]byte("hello world")); err != nil {
+			http.Error(w, "failed to write response", http.StatusInternalServerError)
+		}
 	})
-	http.ListenAndServe(":3000", r)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
 }
